2be7432fcf3194d8f2183bc45247c147132dbc59
Mikkopoika <mikko.muilu@gmail.com>
2017-01-29 23:12:16 +0200

    Fixed

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,16 +1,23 @@
 package sec.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import sec.project.config.CustomUserDetailsService;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
 
 @Controller
 public class SignupController {
-
+    
+    
+    private CustomUserDetailsService users = new CustomUserDetailsService();
     @Autowired
     private SignupRepository signupRepository;
 
@@ -19,6 +26,29 @@ public class SignupController {
         return "redirect:/form";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loadLogin() {
+        return "login";
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String submitLogin(@RequestParam String name, @RequestParam String password) {
+        users.loadUserByUsername(name);
+        // This lists the content of repository to the output.
+        List<Signup> nimet = signupRepository.findAll();
+        for (int i = 0; i < nimet.size(); i++) {
+            System.out.println("name: " + nimet.get(i).getName() + " and address:  " + nimet.get(i).getAddress());
+        }
+        
+        return "secret";
+    }
+    
+    
+    @RequestMapping(value = "/secret", method = RequestMethod.GET)
+    public String loadSecret() {
+        return "secret";
+    }
+    
     @RequestMapping(value = "/form", method = RequestMethod.GET)
     public String loadForm() {
         return "form";
@@ -26,8 +56,23 @@ public class SignupController {
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String submitForm(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new Signup(name, address));
+        Signup signup = signupRepository.save(new Signup(name, address));
+        // This lists the content of repository to the output.
+        List<Signup> nimet = signupRepository.findAll();
+        for (int i = 0; i < nimet.size(); i++) {
+            System.out.println("name: " + nimet.get(i).getName() + " and address:  " + nimet.get(i).getAddress());
+        }
+        
+        return "redirect:/done/" + signup.getId();
+        
+    }
+    
+    @RequestMapping(value = "/done/{id}", method = RequestMethod.GET)
+    public String loaddone(Model model, @PathVariable Long id) {
+        model.addAttribute("user", signupRepository.findOne(id));
+        
         return "done";
     }
+    
 
 }
